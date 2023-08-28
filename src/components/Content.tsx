61b48841b523877ece08bbb09d022bbe38a51509
Anderson Martínez <anderson@browserless.io>
2023-08-28 15:28:47 -0400

    feat: adding generic content component

---
+++ src/components/Content.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+import useTheme from "../hooks/useTheme";
+import { classNames } from "../utils";
+const styles = {
+  light: "bg-primary-light",
+  dark: "bg-primary-dark",
+};
+
+const Content = (props: ContentProps) => {
+  const { children, className: additionalClassName } = props;
+  const { theme } = useTheme();
+  const colors = styles[theme];
+
+  return (
+    <div
+      className={classNames(
+        "rounded-md text-base w-full h-full p-2 px-4 mb-4",
+        colors,
+        additionalClassName,
+      )}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Content;
+export interface ContentProps {
+  title?: string;
+  className?: string;
+  children?: React.ReactNode;
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,15 @@
-export { default as Alert } from './components/Alert';
-export { default as Aside } from './components/Aside';
-export { default as AsideItem } from './components/AsideItem';
-export { default as AsideSection } from './components/AsideSection';
-export { default as Button } from './components/Button';
-export { default as Heading } from './components/Heading';
-export { default as Input } from './components/Input';
-export { default as Loader } from './components/Loader';
-export { default as Modal } from './components/Modal';
-export { default as Page } from './components/Page';
+export { default as Alert } from "./components/Alert";
+export { default as Aside } from "./components/Aside";
+export { default as AsideItem } from "./components/AsideItem";
+export { default as AsideSection } from "./components/AsideSection";
+export { default as Button } from "./components/Button";
+export { default as Content } from "./components/Content";
+export { default as Heading } from "./components/Heading";
+export { default as Input } from "./components/Input";
+export { default as Loader } from "./components/Loader";
+export { default as Modal } from "./components/Modal";
+export { default as Page } from "./components/Page";
 
-export { default as useTheme } from './hooks/useTheme';
+export { default as useTheme } from "./hooks/useTheme";
 
-export { default as ThemeProvider, ThemeContext } from './providers/ThemeProvider';
+export { default as ThemeProvider, ThemeContext } from "./providers/ThemeProvider";
