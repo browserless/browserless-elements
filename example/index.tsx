b5500aa9718b7fc47f929b4049cff335607f12c5
Anderson Martínez <anderson@browserless.io>
2023-12-14 10:30:16 -0400

    docs: updating example

--- example/index.tsx
+++ example/index.tsx
@@ -14,7 +14,6 @@ import {
   Heading,
   Button,
   Alert,
-  Loader,
   Select,
   Input,
 } from "../.";
@@ -22,6 +21,45 @@ import {
 import "../dist/elements.css";
 
 type Theme = "light" | "dark";
+
+const ReactIcon = () => (
+  <svg
+    stroke="currentColor"
+    fill="currentColor"
+    stroke-width="0"
+    viewBox="0 0 24 24"
+    height="1em"
+    width="1em"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <circle cx="12" cy="11.245" r="1.785"></circle>
+    <path d="m7.002 14.794-.395-.101c-2.934-.741-4.617-2.001-4.617-3.452 0-1.452 1.684-2.711 4.617-3.452l.395-.1.111.391a19.507 19.507 0 0 0 1.136 2.983l.085.178-.085.178c-.46.963-.841 1.961-1.136 2.985l-.111.39zm-.577-6.095c-2.229.628-3.598 1.586-3.598 2.542 0 .954 1.368 1.913 3.598 2.54.273-.868.603-1.717.985-2.54a20.356 20.356 0 0 1-.985-2.542zm10.572 6.095-.11-.392a19.628 19.628 0 0 0-1.137-2.984l-.085-.177.085-.179c.46-.961.839-1.96 1.137-2.984l.11-.39.395.1c2.935.741 4.617 2 4.617 3.453 0 1.452-1.683 2.711-4.617 3.452l-.395.101zm-.41-3.553c.4.866.733 1.718.987 2.54 2.23-.627 3.599-1.586 3.599-2.54 0-.956-1.368-1.913-3.599-2.542a20.683 20.683 0 0 1-.987 2.542z"></path>
+    <path d="m6.419 8.695-.11-.39c-.826-2.908-.576-4.991.687-5.717 1.235-.715 3.222.13 5.303 2.265l.284.292-.284.291a19.718 19.718 0 0 0-2.02 2.474l-.113.162-.196.016a19.646 19.646 0 0 0-3.157.509l-.394.098zm1.582-5.529c-.224 0-.422.049-.589.145-.828.477-.974 2.138-.404 4.38.891-.197 1.79-.338 2.696-.417a21.058 21.058 0 0 1 1.713-2.123c-1.303-1.267-2.533-1.985-3.416-1.985zm7.997 16.984c-1.188 0-2.714-.896-4.298-2.522l-.283-.291.283-.29a19.827 19.827 0 0 0 2.021-2.477l.112-.16.194-.019a19.473 19.473 0 0 0 3.158-.507l.395-.1.111.391c.822 2.906.573 4.992-.688 5.718a1.978 1.978 0 0 1-1.005.257zm-3.415-2.82c1.302 1.267 2.533 1.986 3.415 1.986.225 0 .423-.05.589-.145.829-.478.976-2.142.404-4.384-.89.198-1.79.34-2.698.419a20.526 20.526 0 0 1-1.71 2.124z"></path>
+    <path d="m17.58 8.695-.395-.099a19.477 19.477 0 0 0-3.158-.509l-.194-.017-.112-.162A19.551 19.551 0 0 0 11.7 5.434l-.283-.291.283-.29c2.08-2.134 4.066-2.979 5.303-2.265 1.262.727 1.513 2.81.688 5.717l-.111.39zm-3.287-1.421c.954.085 1.858.228 2.698.417.571-2.242.425-3.903-.404-4.381-.824-.477-2.375.253-4.004 1.841.616.67 1.188 1.378 1.71 2.123zM8.001 20.15a1.983 1.983 0 0 1-1.005-.257c-1.263-.726-1.513-2.811-.688-5.718l.108-.391.395.1c.964.243 2.026.414 3.158.507l.194.019.113.16c.604.878 1.28 1.707 2.02 2.477l.284.29-.284.291c-1.583 1.627-3.109 2.522-4.295 2.522zm-.993-5.362c-.57 2.242-.424 3.906.404 4.384.825.47 2.371-.255 4.005-1.842a21.17 21.17 0 0 1-1.713-2.123 20.692 20.692 0 0 1-2.696-.419z"></path>
+    <path d="M12 15.313c-.687 0-1.392-.029-2.1-.088l-.196-.017-.113-.162a25.697 25.697 0 0 1-1.126-1.769 26.028 26.028 0 0 1-.971-1.859l-.084-.177.084-.179c.299-.632.622-1.252.971-1.858.347-.596.726-1.192 1.126-1.77l.113-.16.196-.018a25.148 25.148 0 0 1 4.198 0l.194.019.113.16a25.136 25.136 0 0 1 2.1 3.628l.083.179-.083.177a24.742 24.742 0 0 1-2.1 3.628l-.113.162-.194.017c-.706.057-1.412.087-2.098.087zm-1.834-.904c1.235.093 2.433.093 3.667 0a24.469 24.469 0 0 0 1.832-3.168 23.916 23.916 0 0 0-1.832-3.168 23.877 23.877 0 0 0-3.667 0 23.743 23.743 0 0 0-1.832 3.168 24.82 24.82 0 0 0 1.832 3.168z"></path>
+  </svg>
+);
+
+export const timeoutOptions = [
+  { text: "No timeout", value: -1 },
+  { text: "10 seconds", value: 10000 },
+  { text: "15 seconds", value: 15000 },
+  { text: "20 seconds", value: 20000 },
+  { text: "25 seconds", value: 25000 },
+  { text: "30 seconds", value: 30000 },
+  { text: "45 seconds", value: 45000 },
+  { text: "1 minute", value: 60000 },
+  { text: "2 minutes", value: 120000 },
+  { text: "5 minutes", value: 300000 },
+  { text: "10 minutes", value: 600000 },
+  { text: "15 minutes", value: 900000 },
+  { text: "20 minutes", value: 1200000 },
+  { text: "30 minutes", value: 1800000 },
+  { text: "1 hour", value: 3600000 },
+  { text: "6 hours", value: 21600000 },
+  { text: "12 hours", value: 43200000 },
+  { text: "1 day", value: 86400000 },
+];
 
 const App = () => {
   if (!localStorage.getItem("theme")) {
@@ -116,6 +154,7 @@ const App = () => {
             <Button>Default</Button>
             <Button disabled>Disabled</Button>
             <Button busy>Busy</Button>
+            <Button prefix={<ReactIcon />}>Prefixed</Button>
           </div>
 
           <div
@@ -131,6 +170,9 @@ const App = () => {
             </Button>
             <Button actionType="danger" busy>
               Danger Busy
+            </Button>
+            <Button actionType="danger" prefix={<ReactIcon />}>
+              Prefixed
             </Button>
           </div>
 
@@ -148,6 +190,9 @@ const App = () => {
             <Button actionType="warning" busy>
               Warning Busy
             </Button>
+            <Button actionType="warning" prefix={<ReactIcon />}>
+              Prefixed
+            </Button>
           </div>
         </>
         <br />
@@ -163,13 +208,17 @@ const App = () => {
               width: "40%",
               display: "flex",
               flexDirection: "column",
-              gap: "0.5em",
+              gap: "1em",
             }}
           >
             <Input label="Default" />
             <Input label="Placeholder" placeholder="Placeholder" />
             <Input label="Required" required />
             <Input label="Input Type" type="date" />
+            <Input
+              label="With Description"
+              description="This input field has a description. That can be useful."
+            />
             <Input label="Disabled" disabled />
           </div>
         </>
@@ -207,6 +256,14 @@ const App = () => {
               required
             />
             <Select
+              label="Lots of elements"
+              values={Array.from({ length: 25 }).map((_, i) => ({
+                value: i,
+                text: `Option ${i + 1}`,
+              }))}
+              required
+            />
+            <Select
               label="Disabled"
               values={[
                 { value: 1, text: "Red" },
@@ -239,17 +296,25 @@ const App = () => {
             <Alert type="warning">This is a warning alert</Alert>
 
             <Alert type="info">
-              You may cancel at any time by clicking the "Cancel Account" button
-              below. This will prevent any further charges and your browserless
-              instances will shut down at the end of the billing period.
+              Multiple lines of text can be used in an alert. Lorem ipsum dolor sit
+              amet consectetur adipisicing elit. Harum vero alias necessitatibus
+              numquam doloremque sit, tempora aperiam dolores animi nostrum officiis
+              neque, dolorum saepe ad possimus. Laudantium corrupti magni
+              accusantium.
             </Alert>
           </div>
         </>
 
-        {/* <Loader />
-        <p style={{ marginTop: "5em" }}>
-          (Just in case, this <i>won&apos;t</i> finish loading...)
-        </p> */}
+        <Heading size="lg">Headings</Heading>
+        <br />
+        <br />
+        <Heading size="2xl">2XL Heading</Heading>
+        <Heading size="xl">XL Heading</Heading>
+        <Heading size="lg">Large Heading</Heading>
+        <Heading size="md">Medium Heading</Heading>
+        <Heading size="sm">Small Heading</Heading>
+        <br />
+        <br />
       </Page>
     </ThemeProvider>
   );
