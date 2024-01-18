421194e660ea8b6857ab90ee20f9162fffefca68
Anderson Martínez <anderson@browserless.io>
2024-01-18 10:51:22 -0400

    fix: roll back useOuterClick hook chnges

--- src/hooks/useOuterClick.tsx
+++ src/hooks/useOuterClick.tsx
@@ -1,29 +1,33 @@
 import React from "react";
 
-export default function useOuterClick<T extends HTMLElement>(callback?: Function) {
-  const innerRef = React.useRef<T>(null);
-  const callbackRef = React.useRef(callback);
+export default function useOuterClick(callback?: Function) {
+  const innerRef = React.useRef();
+  const callbackRef = React.useRef();
 
   // set current callback in ref, before second useEffect uses it
   React.useEffect(() => {
+    // useEffect wrapper to be safe for concurrent mode
+    // @ts-ignore
     callbackRef.current = callback;
-  }, [callback]);
+  });
 
   React.useEffect(() => {
     document.addEventListener("click", handleClick);
     return () => document.removeEventListener("click", handleClick);
 
     // read most recent callback and innerRef dom node from refs
-    function handleClick(e: MouseEvent) {
+    function handleClick(e: any) {
       if (
         innerRef.current &&
         callbackRef.current &&
-        !innerRef.current.contains(e.currentTarget as Node)
+        // @ts-ignore
+        !innerRef.current.contains(e.target)
       ) {
+        // @ts-ignore
         callbackRef.current(e);
       }
     }
   }, []); // no need for callback + innerRef dep
 
-  return innerRef; // return ref; client can omit `useRef`
+  return innerRef as unknown as React.LegacyRef<HTMLDivElement>; // return ref; client can omit `React.useRef`
 }
